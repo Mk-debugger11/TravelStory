3837bb53fb83976cdb096c35b2838922f2d9112d
Mukul Kumar <mukulkumarsharma540@gmail.com>
2025-05-05 10:37:02 +0530

    fixed video color bug

--- src/video/videoAnimation.jsx
+++ src/video/videoAnimation.jsx
@@ -1,4 +1,4 @@
-import { useCurrentFrame, interpolate, Img} from 'remotion';
+import { useCurrentFrame, interpolate, Img } from 'remotion';
 import React from 'react';
 import { AbsoluteFill } from 'remotion';
 export const VideoAnimation = ({ elements }) => {
@@ -7,54 +7,53 @@ export const VideoAnimation = ({ elements }) => {
   return (
     <AbsoluteFill style={{ backgroundColor: '#e3e3e3' }}>
       <div style={{
-      flex: 1,
-      fontFamily: 'sans-serif',
-      position:'relative'
-    }}>
-      {elements.map((item, index) => {
-        const delay = index * 30;
-        const enterStart = delay;
-        const enterEnd = delay + 20;
+        flex: 1,
+        fontFamily: 'sans-serif',
+        position: 'relative'
+      }}>
+        {elements.map((item, index) => {
+          const delay = index * 30;
+          const enterStart = delay;
+          const enterEnd = delay + 20;
 
-        const opacity = interpolate(frame, [enterStart, enterEnd], [0, 1], {
-          extrapolateLeft: 'clamp',
-          extrapolateRight: 'clamp',
-        });
+          const opacity = interpolate(frame, [enterStart, enterEnd], [0, 1], {
+            extrapolateLeft: 'clamp',
+            extrapolateRight: 'clamp',
+          });
 
-        const scale = interpolate(frame, [enterStart, enterEnd], [0.5, 1], {
-          extrapolateLeft: 'clamp',
-          extrapolateRight: 'clamp',
-        });
+          const scale = interpolate(frame, [enterStart, enterEnd], [0.5, 1], {
+            extrapolateLeft: 'clamp',
+            extrapolateRight: 'clamp',
+          });
 
-        const translateY = interpolate(frame, [enterStart, enterEnd], [0,item.position.y], {
-          extrapolateLeft: 'clamp',
-          extrapolateRight: 'clamp',
-        });
-        const translateX = interpolate(frame,[enterStart,enterEnd],[0,item.position.x],{
-            extrapolateLeft:'clamp',
-            extrapolateRight:'clamp'
-        })
-        return (
-          <div
-            key={index}
-            style={{
-              opacity,
-              transform: `scale(${scale}) translateY(${translateY}px) translateX(${translateX}px)`,
-              color: 'white',
-              fontSize: item.type === 'text' ? 16 : 'auto',
-              position:'absolute'
-            }}
-          >
-            {item.type === 'text' ? (
-              <div>{item.content}</div>
-            ) : (
-              <Img src={item.content} style={{ width: 320, borderRadius: 16 }} />
-            )}
-          </div>
-        );
-      })}
-    </div>
+          const translateY = interpolate(frame, [enterStart, enterEnd], [0, item.position.y], {
+            extrapolateLeft: 'clamp',
+            extrapolateRight: 'clamp',
+          });
+          const translateX = interpolate(frame, [enterStart, enterEnd], [0, item.position.x], {
+            extrapolateLeft: 'clamp',
+            extrapolateRight: 'clamp'
+          })
+          return (
+            <div
+              key={index}
+              style={{
+                opacity,
+                transform: `scale(${scale}) translateY(${translateY}px) translateX(${translateX}px)`,
+                position: 'absolute',
+                color: 'white',
+                fontSize: item.type === 'text' ? 16 : 'auto',
+              }}
+            >
+              {item.type === 'text' ? (
+                <div style={{ color: item.color, fontSize: item.size }}>{item.content}</div>
+              ) : (
+                <Img src={item.content} style={{ width: 320, borderRadius: 16 }} />
+              )}
+            </div>
+          );
+        })}
+      </div>
     </AbsoluteFill>
-    
   );
 };
